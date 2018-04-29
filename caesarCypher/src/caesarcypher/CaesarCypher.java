d80a63a7de19d5deb11c1a7c954d991401c47993
omgKyleyay <1337scriptdoggy@gmail.com>
2018-04-28 23:31:00 -0400

    Caesar Text Encryption

---
+++ caesarCypher/src/caesarcypher/CaesarCypher.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package caesarcypher;
+
+/**
+ *
+ * @author Kyle
+ */
+public class CaesarCypher {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CypherGUI f = new CypherGUI();
+        f.setVisible(true);
+    }
+    
+}
